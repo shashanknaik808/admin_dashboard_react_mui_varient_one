928f4c048341ebc465d09dfd3578fcd361313a91
Shashank Naik <shashanknaik808@gmail.com>
2023-12-29 15:45:50 +0530

    Refactor Sidenav component to use MUI styling and components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import { Box } from '@mui/material';
 import React from 'react';
+import Sidenav from '../Sidenav';
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <>
+            <Box sx={{ display: 'flex' }}>
+                <Sidenav />
+                <h1>Home</h1>
+            </Box>
+        </>
     )
 }
