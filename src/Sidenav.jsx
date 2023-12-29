345e4836165bb1d58dea9a2961b333c16883eaf1
Shashank Naik <shashanknaik808@gmail.com>
2023-12-29 15:04:15 +0530

    Creating Sidenav Component

---
+++ src/Sidenav.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Sidenav extends Component {
+    render() {
+        return (
+            <div>Sidenav</div>
+        )
+    }
+}
+
+export default Sidenav;
