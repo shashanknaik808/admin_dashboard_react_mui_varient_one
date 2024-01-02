faa26e5bfafc822c8c5b979772248a22f03f9776
Shashank Naik <shashanknaik808@gmail.com>
2024-01-02 17:59:52 +0530

    Integrate Navbar in About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Sidenav from '../components/Sidenav';
 import { Box } from '@mui/material';
+import Navbar from '../components/Navbar';
 
 export default function About() {
     return (
         <>
+            <Navbar />
+            <Box height={30} />
             <Box sx={{ display: 'flex' }}>
                 <Sidenav />
                 <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
