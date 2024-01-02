1d481f61f439bac31135a3b2b33dd91424be2f7f
Shashank Naik <shashanknaik808@gmail.com>
2024-01-02 17:46:43 +0530

    Integrated Navbar into Settings Page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Sidenav from '../components/Sidenav';
 import { Box } from '@mui/material';
+import Navbar from '../components/Navbar';
 
 export default function Settings() {
     return (
         <>
+            <Navbar />
+            <Box height={30} />
             <Box sx={{ display: 'flex' }}>
                 <Sidenav />
                 <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
