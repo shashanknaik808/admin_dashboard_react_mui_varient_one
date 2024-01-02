82dc7046a786b1c5ae82f0dc4eaa2e72f54c489f
Shashank Naik <shashanknaik808@gmail.com>
2024-01-02 18:07:50 +0530

    Adding Home Page Icons

---
+++ src/components/AccordionDash.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Sidenav from '../components/Sidenav.jsx'
+import { Box, Card, CardContent, Stack, Typography } from '@mui/material'
+import Navbar from '../components/Navbar.jsx';
+import Grid from '@mui/material/Grid';
+import '../Dash.css';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import AccordionDash from '../components/AccordionDash.jsx';
+export default function Home() {
+  return (
+    <>
+      <div className='bgcolor'>
+        <Navbar />
+        <Box height={70} />
+        <Box sx={{ display: "flex" }}>
+          <Sidenav />
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={8}>
+                <Stack spacing={2} direction={"row"}>
+                  <Card sx={{ minWidth: 49 + "%", height: 150 }} className='gradient'>
+                    <CardContent>
+                      <div className='iconstyle'>
+                        <CreditCardIcon />
+                      </div>
+                      <Typography gutterBottom variant="h5" component="div" sx={{ color: "#ffffff" }}>
+                        $500.00
+                      </Typography>
+                      <Typography gutterBottom variant="body2" component="div" sx={{ color: "#ccd1d1" }}>
+                        Total Earnings
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                  <Card sx={{ minWidth: 49 + "%", height: 150 }} className='gradientlight'>
+                    <CardContent>
+                      <div className='iconstyle'>
+                        <ShoppingBagIcon />
+                      </div>
+                      <Typography gutterBottom variant="h5" component="div" sx={{ color: "#ffffff" }}>
+                        $900.00
+                      </Typography>
+                      <Typography gutterBottom variant="body2" component="div" sx={{ color: "#ccd1d1" }}>
+                        Total Orders
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Stack>
+              </Grid>
+              <Grid item xs={4}>
+                <Stack spacing={2} >
+                  <Card className='gradientlight'>
+                    <Stack spacing={2} direction={"row"} >
+                      <div className='iconstyle'>
+                        <StorefrontIcon />
+                      </div>
+                      <div className='paddingall'>
+                        <span className='pricetitle'>$203k</span>
+                        <br />
+                        <span className='pricesubtitle'> Total Income</span>
+                      </div>
+                    </Stack>
+                  </Card>
+                  <Card >
+                    <Stack spacing={2} direction={"row"} >
+                      <div className='iconstyleblack'>
+                        <StorefrontIcon />
+                      </div>
+                      <div className='paddingall'>
+                        <span className='pricetitle'>$203k</span>
+                        <br />
+                        <span className='pricesubtitle'> Total Income</span>
+                      </div>
+                    </Stack>
+                  </Card>
+                </Stack>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </div>
+    </>
+  )
+}
