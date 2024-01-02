ea0eed2cc77a46286869430a29b6d6f1a26cccc1
Shashank Naik <shashanknaik808@gmail.com>
2024-01-02 17:44:14 +0530

    Added Navbar to Home component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidenav from '../Sidenav';
+import Sidenav from '../components/Sidenav';
 import { Box } from '@mui/material';
 
 export default function About() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Sidenav from '../Sidenav';
+import Sidenav from '../components/Sidenav';
 import { Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import Navbar from '../components/Navbar';
+
 export default function Home() {
     return (
         <>
+            <Navbar />
+            <Box height={30} />
             <Box sx={{ display: 'flex' }}>
                 <Sidenav />
                 <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
