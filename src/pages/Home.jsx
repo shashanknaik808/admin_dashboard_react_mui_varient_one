a0aeac286b9fb867dd14a2400e197af2d3ffdaf7
Shashank Naik <shashanknaik808@gmail.com>
2024-04-03 23:37:21 +0530

    Updated Home component layout with additional Card components and adjusted Grid and Stack spacing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Sidenav from '../components/Sidenav.jsx';
 import Navbar from '../components/Navbar.jsx';
-import { Box, Card, CardContent, Typography } from '@mui/material';
+import { Box, Card, CardContent, Stack, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
 
@@ -15,26 +15,57 @@ export default function Home() {
                 <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                     <Grid container spacing={2}>
                         <Grid item xs={8}>
-                            <Card sx={{ maxWidth: 345 }}>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Lizard
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                                        species, ranging across all continents except Antarctica
-                                    </Typography>
-                                </CardContent>
-                            </Card>
+                            <Stack spacing={2} direction="row">
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                                            species, ranging across all continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                                            species, ranging across all continents except Antarctica
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Stack>
                         </Grid>
                         <Grid item xs={4}>
+                            <Stack spacing={2}>
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardContent>
+                                    </CardContent>
+                                </Card>
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardContent>
+                                    </CardContent>
+                                </Card>
+                            </Stack>
                         </Grid>
                     </Grid>
                     <Box height={20} />
                     <Grid container spacing={2}>
                         <Grid item xs={8}>
+                            <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
+                                <CardContent>
+                                </CardContent>
+                            </Card>
                         </Grid>
                         <Grid item xs={4}>
+                            <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
+                                <CardContent>
+                                </CardContent>
+                            </Card>
                         </Grid>
                     </Grid>
                 </Box>
