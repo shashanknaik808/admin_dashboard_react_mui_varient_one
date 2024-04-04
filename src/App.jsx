a0b83c84b232c7b707ac1bfb881a0f171bfd138d
Shashank Naik <shashanknaik808@gmail.com>
2024-04-04 21:09:47 +0530

    Added routing functionality using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Settings from './pages/Settings';
+import Analytics from './pages/Analytics';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
           <Route path='/' exact element={<Home />}></Route>
           <Route path='/about' exact element={<About />}></Route>
           <Route path='/settings' exact element={<Settings />}></Route>
+          <Route path='/analytics' exact element={<Analytics />} />
         </Routes>
       </BrowserRouter>
     </>
