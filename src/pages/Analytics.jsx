8912f4b258f3a5fe50325673c1367d7a3d79c1b5
Shashank Naik <shashanknaik808@gmail.com>
2024-04-04 21:15:17 +0530

    Added Analytics page with visitor cards, horizontal bar chart, geo chart, and pie chart

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,19 +1,109 @@
-import React from 'react';
-import Sidenav from '../components/Sidenav';
-import Navbar from '../components/Navbar';
-import { Box } from '@mui/material';
+import React from 'react'
+import Sidenav from '../components/Sidenav.jsx'
+import { Box, Card, CardContent, Stack, Typography } from '@mui/material'
+import Navbar from '../components/Navbar.jsx';
+import Grid from '@mui/material/Grid';
+import '../Dash.css';
+import GeoChart from '../charts/GeoChart.jsx';
+import PiChart from '../charts/PiChart.jsx';
+import HbarChart from '../charts/HbarChart.jsx';
 
 export default function Analytics() {
     return (
         <>
-            <Navbar />
-            <Box height={30} />
-            <Box sx={{ display: 'flex' }}>
-                <Sidenav />
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <h1>Analytics</h1>
+            <div className='bgcolor'>
+                <Navbar />
+                <Box height={70} />
+                <Box sx={{ display: "flex" }}>
+                    <Sidenav />
+                    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={5}>
+                                <Stack spacing={2} direction={"row"}>
+                                    <Box sx={{ width: "50%" }}>
+                                        <Card sx={{ height: 19 + "vh" }} className='gradient'>
+                                            <CardContent>
+                                                <Typography gutterBottom variant="p" component="div" sx={{ color: "#f0fcfc", padding: "4px 0px" }}>
+                                                    Visitors
+                                                </Typography>
+                                                <Typography gutterBottom variant="h5" component="div" sx={{ color: "#f0fcfc" }}>
+                                                    24,630
+                                                </Typography>
+                                                <Typography gutterBottom variant="body2" color="text.secondary" sx={{ color: "#ccd1d1" }}>
+                                                    Since Last week
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card sx={{ height: 19 + "vh", marginTop: "16px" }} className='gradient'>
+                                            <CardContent>
+                                                <Typography gutterBottom variant="p" component="div" sx={{ color: "#f0fcfc", padding: "4px 0px" }}>
+                                                    Visitors
+                                                </Typography>
+                                                <Typography gutterBottom variant="h5" component="div" sx={{ color: "#f0fcfc" }}>
+                                                    24,630
+                                                </Typography>
+                                                <Typography gutterBottom variant="body2" color="text.secondary" sx={{ color: "#ccd1d1" }}>
+                                                    Since Last week
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Box>
+                                    <Box sx={{ width: "50%", height: "50%" }}>
+                                        <Card sx={{ height: 19 + "vh" }} className='gradientlight'>
+                                            <CardContent>
+                                                <Typography gutterBottom variant="p" component="div" sx={{ color: "#f0fcfc", padding: "4px 0px" }}>
+                                                    Visitors
+                                                </Typography>
+                                                <Typography gutterBottom variant="h5" component="div" sx={{ color: "#f0fcfc" }}>
+                                                    24,630
+                                                </Typography>
+                                                <Typography gutterBottom variant="body2" color="text.secondary" sx={{ color: "#ccd1d1" }}>
+                                                    Since Last week
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                        <Card sx={{ height: 19 + "vh", marginTop: "16px" }} className='gradientlight'>
+                                            <CardContent>
+                                                <Typography gutterBottom variant="p" component="div" sx={{ color: "#f0fcfc", padding: "4px 0px" }}>
+                                                    Visitors
+                                                </Typography>
+                                                <Typography gutterBottom variant="h5" component="div" sx={{ color: "#f0fcfc" }}>
+                                                    24,630
+                                                </Typography>
+                                                <Typography gutterBottom variant="body2" color="text.secondary" sx={{ color: "#ccd1d1" }}>
+                                                    Since Last week
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Box>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={7}>
+                                <Card sx={{ height: 40 + "vh" }}>
+                                    <CardContent><HbarChart /></CardContent>
+                                </Card>
+                            </Grid >
+                        </Grid>
+                        <Box height={16} />
+                        <Grid container spacing={2}>
+                            <Grid item xs={8}>
+                                <Card sx={{ height: 40 + "vh" }}>
+                                    <CardContent>
+                                        <GeoChart />
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Card sx={{ height: 40 + "vh" }}>
+                                    <CardContent>
+                                        <PiChart />
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
+                    </Box>
                 </Box>
-            </Box>
+            </div>
         </>
     );
 }
