abbd07d1c1645984132d513200318fa8130b5a97
Shashank Naik <shashanknaik808@gmail.com>
2024-04-05 00:03:44 +0530

    Integrated List component into Settings page with Navbar, Sidenav, and styling

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import Sidenav from '../components/Sidenav';
 import { Box } from '@mui/material';
 import Navbar from '../components/Navbar';
+import List from '../settings/List';
 
 export default function Settings() {
     return (
         <>
-            <Navbar />
-            <Box height={30} />
-            <Box sx={{ display: 'flex' }}>
-                <Sidenav />
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <h1>Settings</h1>
+            <div className='bgcolor'>
+                <Navbar />
+                <Box height={70} />
+                <Box sx={{ display: 'flex' }}>
+                    <Sidenav />
+                    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                        <List />
+                    </Box>
                 </Box>
-            </Box>
+            </div>
         </>
     );
 }
