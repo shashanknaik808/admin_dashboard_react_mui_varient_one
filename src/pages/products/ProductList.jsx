762d31c352886bdf67af09827d0f24e0992133a9
Shashank Naik <shashanknaik808@gmail.com>
2024-04-04 23:55:04 +0530

    Updated ProductList component to include Modal for AddProduct and EditProduct functionalities

--- src/pages/products/ProductList.jsx
+++ src/pages/products/ProductList.jsx
@@ -26,12 +26,37 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import Swal from "sweetalert2";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import Modal from '@mui/material/Modal';
+import AddProduct from './AddProduct.jsx';
+import { useAppStore } from '../../appStore.jsx';
+import EditProduct from './EditProduct.jsx';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 export default function ProductList() {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
-    const [rows, setRows] = useState([]);
+    // const [rows, setRows] = useState([]);
     const empCollectionRef = collection(db, "products");
+    const [open, setOpen] = useState(false);
+    const [editopen, setEditOpen] = useState(false);
+    const [formid, setFormid] = useState("");
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const handleEditOpen = () => setEditOpen(true);
+    const handleEditClose = () => setEditOpen(false);
+    const setRows = useAppStore((state) => state.setRows)
+    const rows = useAppStore((state) => state.rows)
 
     useEffect(() => {
         getUsers();
@@ -83,132 +108,167 @@ export default function ProductList() {
         }
     };
 
+    const editData = (id, name, price, category) => {
+        const data = {
+            id: id,
+            name: name,
+            price: price,
+            category: category,
+        }
+        setFormid(data);
+        handleEditOpen();
+    }
+
     return (
-        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-            <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                sx={{ padding: "20px" }}
-            >
-                Products List
-            </Typography>
-            <Divider />
-            <Box height={10} />
-            <Stack direction="row" spacing={2} className="my-2 mb-2">
-                <Autocomplete
-                    disablePortal
-                    id="combo-box-demo"
-                    options={rows}
-                    sx={{ width: 300 }}
-                    onChange={(e, v) => filterData(v)}
-                    getOptionLabel={(rows) => rows.name || ""}
-                    renderInput={(params) => (
-                        <TextField {...params} size="small" label="Search Products" />
-                    )}
-                />
+        <>
+            <div>
+                <Modal
+                    open={open}
+                    /* onClose={handleClose}*/
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <EditProduct closeEvent={handleClose} />
+                    </Box>
+                </Modal>
+                <Modal
+                    open={editopen}
+                    /* onClose={handleClose}*/
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <EditProduct closeEvent={handleEditClose} fid={formid} />
+                    </Box>
+                </Modal>
+            </div>
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
                 <Typography
-                    variant="h6"
+                    gutterBottom
+                    variant="h5"
                     component="div"
-                    sx={{ flexGrow: 1 }}
-                ></Typography>
-                <Button variant="contained" endIcon={<AddCircleIcon />}>
-                    Add
-                </Button>
-            </Stack>
-            <Box height={10} />
-            <TableContainer sx={{ maxHeight: 440 }}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell
-                                align={"left"}
-                                style={{ minWidth: "100px" }}
-                            >
-                                Name
-                            </TableCell>
-                            <TableCell
-                                align={"left"}
-                                style={{ minWidth: "100px" }}
-                            >
-                                Price
-                            </TableCell>
-                            <TableCell
-                                align={"left"}
-                                style={{ minWidth: "100px" }}
-                            >
-                                Category
-                            </TableCell>
-                            <TableCell
-                                align={"left"}
-                                style={{ minWidth: "100px" }}
-                            >
-                                Date
-                            </TableCell>
-                            <TableCell
-                                align={"left"}
-                                style={{ minWidth: "100px" }}
-                            >
-                                Action
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => {
-                                return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} >
-                                        <TableCell key={row.id} align={"left"}>
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell key={row.id} align={"left"}>
-                                            {row.price}
-                                        </TableCell>
-                                        <TableCell key={row.id} align={"left"}>
-                                            {row.category}
-                                        </TableCell>
-                                        <TableCell key={row.id} align={"left"}>
-                                            {row.date}
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            <Stack spacing={2} direction="row">
-                                                <EditIcon
-                                                    style={{
-                                                        fontSize: "20px",
-                                                        color: "blue",
-                                                        cursor: "pointer",
-                                                    }}
-                                                    className="cursor-pointer"
-                                                // onClick={() => editUser(row.id)}
-                                                />
-                                                <DeleteIcon
-                                                    style={{
-                                                        fontSize: "20px",
-                                                        color: "darkred",
-                                                        cursor: "pointer",
-                                                    }}
-                                                    onClick={() => {
-                                                        deleteUser(row.id);
-                                                    }}
-                                                />
-                                            </Stack>
-                                        </TableCell>
-                                    </TableRow>
-                                );
-                            })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25, 100]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
+                    sx={{ padding: "20px" }}
+                >
+                    Products List
+                </Typography>
+                <Divider />
+                <Box height={10} />
+                <Stack direction="row" spacing={2} className="my-2 mb-2">
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={rows}
+                        sx={{ width: 300 }}
+                        onChange={(e, v) => filterData(v)}
+                        getOptionLabel={(rows) => rows.name || ""}
+                        renderInput={(params) => (
+                            <TextField {...params} size="small" label="Search Products" />
+                        )}
+                    />
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    ></Typography>
+                    <Button variant="contained" endIcon={<AddCircleIcon />} onClick={handleOpen}>
+                        Add
+                    </Button>
+                </Stack>
+                <Box height={10} />
+                <TableContainer sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell
+                                    align={"left"}
+                                    style={{ minWidth: "100px" }}
+                                >
+                                    Name
+                                </TableCell>
+                                <TableCell
+                                    align={"left"}
+                                    style={{ minWidth: "100px" }}
+                                >
+                                    Price
+                                </TableCell>
+                                <TableCell
+                                    align={"left"}
+                                    style={{ minWidth: "100px" }}
+                                >
+                                    Category
+                                </TableCell>
+                                <TableCell
+                                    align={"left"}
+                                    style={{ minWidth: "100px" }}
+                                >
+                                    Date
+                                </TableCell>
+                                <TableCell
+                                    align={"left"}
+                                    style={{ minWidth: "100px" }}
+                                >
+                                    Action
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row) => {
+                                    return (
+                                        <TableRow hover role="checkbox" tabIndex={-1} >
+                                            <TableCell key={row.id} align={"left"}>
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell key={row.id} align={"left"}>
+                                                {row.price}
+                                            </TableCell>
+                                            <TableCell key={row.id} align={"left"}>
+                                                {row.category}
+                                            </TableCell>
+                                            <TableCell key={row.id} align={"left"}>
+                                                {row.date}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                <Stack spacing={2} direction="row">
+                                                    <EditIcon
+                                                        style={{
+                                                            fontSize: "20px",
+                                                            color: "blue",
+                                                            cursor: "pointer",
+                                                        }}
+                                                        className="cursor-pointer"
+                                                        onClick={() => editData(row.id, row.name, row.price, row.category)}
+                                                    />
+                                                    <DeleteIcon
+                                                        style={{
+                                                            fontSize: "20px",
+                                                            color: "darkred",
+                                                            cursor: "pointer",
+                                                        }}
+                                                        onClick={() => {
+                                                            deleteUser(row.id);
+                                                        }}
+                                                    />
+                                                </Stack>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25, 100]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </>
     );
 }
